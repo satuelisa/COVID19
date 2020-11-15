5a466b354067149dd5f3fc73860dceb6fc362147
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2020-11-14 19:16:59 -0600

    New data and an OS detector

--- sannico.sh
+++ sannico.sh
@@ -3,4 +3,13 @@ python3 sannico.py > sannico.dat
 tail -n 7 sannico.dat | awk '{printf("%d %s\n", NR, $5)}' > trend.dat
 gnuplot sannico.plot
 convert -density 200 sannico.eps sannico.png
-open sannico.eps
+if [ "$(uname)" == "Darwin" ]; then
+    open sannico.eps
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    gv sannico.eps
+else
+    echo 'I have no plans to use this in Win10'
+    exit
+fi
+
+
